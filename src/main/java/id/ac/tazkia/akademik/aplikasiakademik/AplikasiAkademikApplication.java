34197789b191718c7b43c72e6a88b0a4f6aa56e1
adisuprayogi <suprayogi@tazkia.ac.id>
2018-03-27 16:31:49 +0700

    pertama1

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/AplikasiAkademikApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.akademik.aplikasiakademik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiAkademikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiAkademikApplication.class, args);
+	}
+}
