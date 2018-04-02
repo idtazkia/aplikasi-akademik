ca0e925c7e9380f29b3449f95977f31963ef0aa5
adisuprayogi <suprayogi@tazkia.ac.id>
2018-04-02 17:21:04 +0700

    pasang template bootstrap

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/DashboardController.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/DashboardController.java
@@ -1,4 +1,14 @@
 package id.ac.tazkia.akademik.aplikasiakademik.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public void daftarDashboard(){
+
+    }
+
 }
