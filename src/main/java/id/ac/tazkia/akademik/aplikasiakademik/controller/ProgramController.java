34edfa56de4f50849078770f74200309af8dd2d9
Haffizh <haffizh@tazkia.ac.id>
2018-06-25 14:06:12 +0700

    screen program (form dan list)

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/ProgramController.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.akademik.aplikasiakademik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProgramController {
+    @GetMapping("/program/list")
+    public void daftarProgram(){
+    }
+
+}
