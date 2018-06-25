bd7ec9e4d4bf5ad043c9a360dcb4146c7246d026
gifar <gifar@tazkia.ac.id>
2018-06-25 16:44:36 +0700

    screen akademik list

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/AkademikController.java
@@ -0,0 +1,11 @@
+package id.ac.tazkia.akademik.aplikasiakademik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AkademikController {
+
+    @GetMapping("/akademik/list")
+    public void akademikList(){}
+}
