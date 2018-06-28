1247491ef69dbf5e3d25a4f78749ecb6ff9d918b
Haffizh <haffizh@tazkia.ac.id>
2018-06-28 09:56:30 +0700

    screen form prasyarat

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/PrasyaratController.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.akademik.aplikasiakademik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrasyaratController {
+
+    @GetMapping("/prasyarat/form")
+    public void formPrasyarat(){
+    }
+}
