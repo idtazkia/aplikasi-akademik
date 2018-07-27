f36058e6435b53caae79df55b08bac4bc7467994
dimashadiwibowo <dimashadiwibowo13@gmail.com>
2018-07-27 14:33:57 -0400

    kampusdao

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KampusDao.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KampusDao.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface KampusDao extends PagingAndSortingRepository<Kampus, String> {
     Page<Kampus> findByStatus (String status, Pageable page);
+    List<Kampus> findByStatus (String status);
 
 }
