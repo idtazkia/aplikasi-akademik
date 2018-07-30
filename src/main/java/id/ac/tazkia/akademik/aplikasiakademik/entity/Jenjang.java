d54a37649087dabce7ca11bb8277d87ecb366f86
dimashadiwibowo <dimashadiwibowo13@gmail.com>
2018-07-30 10:00:33 -0400

    entity jenjang

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/entity/Jenjang.java
@@ -0,0 +1,51 @@
+package id.ac.tazkia.akademik.aplikasiakademik.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "tb__jenjang")
+public class Jenjang {
+
+    @Id
+    @GeneratedValue(generator = "uuid" )
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String idJenjang;
+
+    @NotNull
+    private String kodeJenjang;
+
+    @NotNull
+    private String namaJenjang;
+
+    @NotNull
+    private String keterangan;
+
+    @NotNull
+    private String na;
+
+    private String status;
+
+    @Column(columnDefinition = "DATE")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime tglInsert;
+
+    @Column(columnDefinition = "DATE")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime tglEdit;
+
+    @ManyToOne
+    @JoinColumn(name = "user_insert")
+    private User userInsert;
+
+    @ManyToOne
+    @JoinColumn(name = "user_edit ")
+    private User userEdit;
+
+}
