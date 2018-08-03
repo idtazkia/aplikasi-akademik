ff6aa18bc2ec5970bc36d848ba3ceb934cab3a7c
dimashadiwibowo <dimashadiwibowo13@gmail.com>
2018-08-03 08:34:39 -0400

    prodi entity&dao

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/entity/Prodi.java
@@ -0,0 +1,64 @@
+package id.ac.tazkia.akademik.aplikasiakademik.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+    @Entity
+    @Data
+    @Table(name = "tb_prodi")
+    public class Prodi {
+
+        @Id
+        @GeneratedValue(generator = "uuid" )
+        @GenericGenerator(name = "uuid", strategy = "uuid2")
+        private String idProdi;
+
+        @ManyToOne
+        @NotNull
+        @JoinColumn(name = "id_jurusan")
+        private Jurusan idJurusan;
+
+        @ManyToOne
+        @NotNull
+        @JoinColumn(name = "id_jenjang")
+        private Jurusan idJenjang;
+
+        @NotNull
+        private String kodeProdi;
+
+        @NotNull
+        private String namaProdi;
+
+
+        @NotNull
+        private String pejabat;
+
+        @NotNull
+        private String na = "1";
+
+        @NotNull
+        private String status = "1";
+
+
+        @Column(columnDefinition = "DATE")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDateTime tglInsert;
+
+        @Column(columnDefinition = "DATE")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDateTime tglEdit;
+
+        @ManyToOne
+        @JoinColumn(name = "user_insert")
+        private User userInsert;
+
+        @ManyToOne
+        @JoinColumn(name = "user_edit ")
+        private User userEdit;
+
+}
