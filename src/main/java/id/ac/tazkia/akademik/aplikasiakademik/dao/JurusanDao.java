3c8f7243a7e0541ff72d2735d591c77ba3d5bd9b
magang04 <mwildansoleh@gmail.com>
2018-08-03 08:34:24 -0400

    jurusan Entity dan Dao

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/JurusanDao.java
@@ -0,0 +1,10 @@
+package id.ac.tazkia.akademik.aplikasiakademik.dao;
+
+import id.ac.tazkia.akademik.aplikasiakademik.entity.Jurusan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface JurusanDao extends PagingAndSortingRepository<Jurusan, String> {
+    Page<Jurusan> findByStatus(String status, Pageable page);
+}
---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/entity/Jurusan.java
@@ -0,0 +1,55 @@
+package id.ac.tazkia.akademik.aplikasiakademik.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Table(name = "tb_jurusan")
+@Entity
+@Data
+public class Jurusan {
+    @Id
+    @GeneratedValue(generator = "uuid" )
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String idJurusan;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "id_fakultas")
+    private Fakultas idFakultas;
+
+    @NotNull
+    private String kodeJurusan;
+
+    @NotNull
+    private String namaJurusan;
+
+    @NotNull
+    private String pejabat;
+
+    @NotNull
+    private String na = "1";
+
+    @NotNull
+    private String status = "1";
+
+    @Column(columnDefinition = "DATE")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime tglInsert;
+
+    @Column(columnDefinition = "DATE")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime tglEdit;
+
+    @ManyToOne
+    @JoinColumn(name = "user_insert")
+    private User userInsert;
+
+    @ManyToOne
+    @JoinColumn(name = "user_edit ")
+    private User userEdit;
+}
