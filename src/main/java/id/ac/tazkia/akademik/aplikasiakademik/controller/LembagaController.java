844445504a25ce3924aa1aae9025ab6af73c0899
gifar <gifar@tazkia.ac.id>
2018-09-21 11:01:04 +0700

    simpan lembaga

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/LembagaController.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/LembagaController.java
@@ -1,6 +1,8 @@
 package id.ac.tazkia.akademik.aplikasiakademik.controller;
 
 import id.ac.tazkia.akademik.aplikasiakademik.dao.LembagaDao;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.Jenjang;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.Lembaga;
 import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class LembagaController {
@@ -28,6 +34,28 @@ public class LembagaController {
     }
 
     @GetMapping("/lembaga/form")
-    public void  formLembaga(){
+    public void  formLembaga(Model model,@RequestParam(required = false) String id){
+        model.addAttribute("lembaga", new Lembaga());
+
+        if (id != null && !id.isEmpty()) {
+            Lembaga lembaga = lembagaDao.findById(id).get();
+            if (lembaga != null) {
+                model.addAttribute("lembaga", lembaga);
+                if (lembaga.getStatus() == null){
+                    lembaga.setStatus(StatusRecord.NONAKTIF);
+                }
+            }
+        }
     }
+
+    @PostMapping("/lembaga/form")
+    public String prosesForm(@Valid Lembaga lembaga){
+        if (lembaga.getStatus() == null){
+            lembaga.setStatus(StatusRecord.NONAKTIF);
+        }
+        lembagaDao.save(lembaga);
+        return "redirect:list";
+    }
+
+
 }
