43f0634e9a45fb93c6bbff3a4ccdca13840a701c
Sawrain <ramadhani@tazkia.ac.id>
2018-10-05 16:59:12 +0700

    Fungsi Karyawan

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KaryawanDao.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KaryawanDao.java
@@ -1,7 +1,17 @@
 package id.ac.tazkia.akademik.aplikasiakademik.dao;
 
 import id.ac.tazkia.akademik.aplikasiakademik.entity.Karyawan;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface KaryawanDao extends PagingAndSortingRepository<Karyawan,String> {
+
+    Page<Karyawan> findByStatus(StatusRecord status, Pageable page);
+    Page<Karyawan> findByStatusAndNamaKaryawanContainingIgnoreCaseOrderByNamaKaryawan(StatusRecord statusRecord, String search, Pageable page);
+
 }
