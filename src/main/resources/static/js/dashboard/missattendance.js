24c8e808ae37f66cc773c804584fd908aeb78852
adisuprayogi <suprayogi@tazkia.ac.id>
2018-10-17 10:22:40 +0700

    Dashboard Grafik Warna

--- src/main/resources/static/js/dashboard/missattendance.js
+++ src/main/resources/static/js/dashboard/missattendance.js
@@ -15,6 +15,7 @@ function generateSuccessHTMLOutput(response) {
     hasil = "";
     for(i=0;i < response.data.length;i++) {
         hasil += generateRowPersentase(response.data[i])
+
     }
     return hasil;
 }
@@ -23,13 +24,18 @@ function generateRowPersentase(data) {
     matkul = data.matakuliah;
     persentase = data.persentase;
     absen = data.absen;
+    if(persentase < 60){
+        warna='bg-orange';
+    }else{
+        warna='bg-red';
+    }
     return '<div class="widget_summary">' +
         '<div class="w_left w_25">' +
         '<span>' + matkul + ' :  ' + '</span>&nbsp;' +
         '</div>' +
         '<div class="w_center w_55">' +
         '<div class="progress">' +
-        '<div class="progress-bar bg-orange" role="progressbar" aria-valuenow="' + persentase + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + persentase + '%;">' +
+        '<div class="progress-bar '+ warna +'" role="progressbar" aria-valuenow="' + persentase + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + persentase + '%;">' +
         '<span class="sr-only">60% Complete</span>' +
         '</div>' +
         '</div>' +
