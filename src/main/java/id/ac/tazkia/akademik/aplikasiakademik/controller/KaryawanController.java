430cb2497effb8e2a21d6babd3fdb6c432f62034
gifar <gifar@tazkia.ac.id>
2018-10-17 08:34:06 +0700

    reset controller karyawan

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/KaryawanController.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/KaryawanController.java
@@ -1,29 +1,15 @@
 package id.ac.tazkia.akademik.aplikasiakademik.controller;
 
-import id.ac.tazkia.akademik.aplikasiakademik.dao.KaryawanDao;
-import id.ac.tazkia.akademik.aplikasiakademik.entity.Karyawan;
-import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class KaryawanController {
 
-
-    @Autowired
-    private KaryawanDao karyawanDao;
-
     @GetMapping("/karyawan/list")
     public void daftarKaryawan(){
 
     }
-
-
 
     @GetMapping("/karyawan/form")
     public void  formKaryawan(){
