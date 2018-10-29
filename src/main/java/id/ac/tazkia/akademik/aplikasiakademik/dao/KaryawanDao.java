ec832532bdffa5b088214b57655091e7f6f40b6b
Sawrain <ramadhani@tazkia.ac.id>
2018-10-29 10:37:51 +0700

    Tambahan Karyawan Dao + Krs Dao

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KaryawanDao.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KaryawanDao.java
@@ -1,9 +1,12 @@
 package id.ac.tazkia.akademik.aplikasiakademik.dao;
 
 import id.ac.tazkia.akademik.aplikasiakademik.entity.Karyawan;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
 import id.ac.tazkia.akademik.aplikasiakademik.entity.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface KaryawanDao extends PagingAndSortingRepository<Karyawan,String> {
+
+    Long countKaryawanByStatus (StatusRecord statusRecord);
 
 }
