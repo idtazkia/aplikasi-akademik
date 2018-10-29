6588d54bc2f283451688f9d4a7bcd2fbed30053d
Sawrain <ramadhani@tazkia.ac.id>
2018-10-29 10:34:32 +0700

    Dosen Dao + Entity

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/DosenDao.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/DosenDao.java
@@ -2,7 +2,11 @@ package id.ac.tazkia.akademik.aplikasiakademik.dao;
 
 
 import id.ac.tazkia.akademik.aplikasiakademik.entity.Dosen;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface DosenDao extends PagingAndSortingRepository<Dosen, String> {
+
+    Long countDosenByStatus(StatusRecord statusRecord);
+
 }
