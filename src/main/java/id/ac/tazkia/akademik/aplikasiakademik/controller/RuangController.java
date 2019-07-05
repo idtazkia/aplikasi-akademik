1acb9695de3b1208d46ab0f90ca8a8460deab1e9
gifar <gifar@tazkia.ac.id>
2019-07-05 14:20:41 +0700

    master ruang dan kelas

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/RuangController.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/controller/RuangController.java
@@ -1,17 +1,16 @@
 package id.ac.tazkia.akademik.aplikasiakademik.controller;
 
-import id.ac.tazkia.akademik.aplikasiakademik.dao.GedungDao;
-import id.ac.tazkia.akademik.aplikasiakademik.dao.RuanganDao;
-import id.ac.tazkia.akademik.aplikasiakademik.dao.RuanganJenisDao;
-import id.ac.tazkia.akademik.aplikasiakademik.entity.Ruangan;
-import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
+import id.ac.tazkia.akademik.aplikasiakademik.dao.*;
+import id.ac.tazkia.akademik.aplikasiakademik.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,6 +27,28 @@ public class RuangController {
 
     @Autowired
     private GedungDao gedungDao;
+
+    @Autowired
+    private MahasiswaDao mahasiswaDao;
+
+    @Autowired
+    private ProdiDao prodiDao;
+
+    @Autowired
+    private KelasDao kelasDao;
+
+    @Autowired
+    private KelasMahasiswaDao kelasMahasiswaDao;
+
+    @ModelAttribute("angkatan")
+    public Iterable<Mahasiswa> angkatan() {
+        return mahasiswaDao.cariAngkatan();
+    }
+
+    @ModelAttribute("prodi")
+    public Iterable<Prodi> prodi() {
+        return prodiDao.findByStatusNotIn(StatusRecord.HAPUS);
+    }
 
     @GetMapping("/ruang/list")
     public void daftarRuang(Model model, @PageableDefault(size = 10) Pageable page, String search){
@@ -71,6 +92,101 @@ public class RuangController {
     public String deleteRuangan(@RequestParam Ruangan ruangan){
         ruangan.setStatus(StatusRecord.HAPUS);
         ruanganDao.save(ruangan);
+
+        return "redirect:list";
+    }
+
+    @GetMapping("/kelas/mahasiswa")
+    public void ruanganMahasiswa(@RequestParam(required = false) String angkatan, @RequestParam Kelas kelas, @RequestParam(required = false) Prodi prodi, Model model, Pageable page){
+        model.addAttribute("selectedKelas", kelas);
+        if (prodi != null){
+            model.addAttribute("selected", prodi);
+            model.addAttribute("selectedAngkatan", angkatan);
+            Iterable<Mahasiswa> mahasiswa = mahasiswaDao.findByStatusAndAngkatanAndIdProdi(StatusRecord.AKTIF,angkatan,prodi);
+            model.addAttribute("mahasiswaList", mahasiswa);
+        }
+    }
+
+    @PostMapping("/kelas/mahasiswa")
+    public String saveruanganMahasiswa(@RequestParam String[] data, @RequestParam Kelas kelas){
+
+        for (String idMahasiswa : data){
+            Mahasiswa mahasiswa = mahasiswaDao.findById(idMahasiswa).get();
+            KelasMahasiswa kelasMahasiswa = kelasMahasiswaDao.findByMahasiswaAndKelas(mahasiswa,kelas);
+            if (kelasMahasiswa != null){
+                KelasMahasiswa kelasMhsw = kelasMahasiswaDao.findByMahasiswaAndStatus(mahasiswa,StatusRecord.AKTIF);
+                if (kelasMhsw != null){
+                    kelasMhsw.setStatus(StatusRecord.NONAKTIF);
+                    kelasMahasiswaDao.save(kelasMhsw);
+                }
+                kelasMahasiswa.setStatus(StatusRecord.AKTIF);
+                kelasMahasiswaDao.save(kelasMahasiswa);
+
+            }else {
+                KelasMahasiswa kelasMhsw = kelasMahasiswaDao.findByMahasiswaAndStatus(mahasiswa,StatusRecord.AKTIF);
+                if (kelasMhsw != null) {
+                    kelasMhsw.setStatus(StatusRecord.NONAKTIF);
+                    kelasMahasiswaDao.save(kelasMhsw);
+                }
+                KelasMahasiswa km = new KelasMahasiswa();
+                km.setStatus(StatusRecord.AKTIF);
+                km.setKelas(kelas);
+                km.setMahasiswa(mahasiswa);
+                kelasMahasiswaDao.save(km);
+            }
+
+
+        }
+        return "redirect:list";
+
+    }
+
+    @GetMapping("/kelas/list")
+    public void daftarKelas(Model model, @PageableDefault(size = 10) Pageable page, String search){
+        if (StringUtils.hasText(search)) {
+            model.addAttribute("search", search);
+            model.addAttribute("listKelas", kelasDao.findByStatusNotInAndNamaKelasContainingIgnoreCaseOrderByNamaKelas(StatusRecord.HAPUS, search, page));
+        } else {
+            model.addAttribute("listKelas",kelasDao.findByStatusNotIn(StatusRecord.HAPUS,page));
+
+        }
+    }
+
+    @GetMapping("/kelas/form")
+    public void  formKelas(Model model,@RequestParam(required = false) String id){
+        model.addAttribute("kelas", new Kelas());
+
+
+        if (id != null && !id.isEmpty()) {
+            Kelas kelas = kelasDao.findById(id).get();
+            if (kelas != null) {
+                model.addAttribute("kelas", kelas);
+                if (kelas.getStatus() == null){
+                    kelas.setStatus(StatusRecord.NONAKTIF);
+                }
+            }
+        }
+    }
+
+    @PostMapping("/kelas/form")
+    public String buatKelas(@Valid Kelas kelas){
+        if (kelas.getStatus() == null){
+            kelas.setStatus(StatusRecord.NONAKTIF);
+        }
+        kelasDao.save(kelas);
+        return "redirect:list";
+    }
+
+    @GetMapping("/kelas/view")
+    public void tampilMahasiswa(@RequestParam Kelas kelas, Model model, Pageable page){
+            Iterable<KelasMahasiswa> kelasMahasiswas = kelasMahasiswaDao.findByKelasAndStatus(kelas,StatusRecord.AKTIF);
+            model.addAttribute("kelasMahasiswa", kelasMahasiswas);
+        }
+
+    @PostMapping("/kelas/delete")
+    public String deleteKelas(@RequestParam Kelas kelas){
+        kelas.setStatus(StatusRecord.HAPUS);
+        kelasDao.save(kelas);
 
         return "redirect:list";
     }
