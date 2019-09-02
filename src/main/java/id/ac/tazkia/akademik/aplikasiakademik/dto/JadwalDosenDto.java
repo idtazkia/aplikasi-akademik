5521e64ff7ec830e6a36878a1744313065d23968
gifar <gifar@tazkia.ac.id>
2019-09-02 10:14:56 +0700

    jadwal dosen dto

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dto/JadwalDosenDto.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dto/JadwalDosenDto.java
@@ -1,20 +1,20 @@
 package id.ac.tazkia.akademik.aplikasiakademik.dto;
 
 
-import id.ac.tazkia.akademik.aplikasiakademik.entity.Dosen;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
-import java.util.Set;
 
 @Data @NoArgsConstructor @AllArgsConstructor
 public class JadwalDosenDto {
+    private String idDosen;
     private Integer absen;
     private String nama;
     private String rfid;
     private String jadwal;
     private LocalTime jamMulai;
     private LocalTime jamSelesai;
+    private Integer jumlah;
 }
