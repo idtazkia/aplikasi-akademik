eb32f9ca79228205b559f0f279429ddd391fe14f
Sawrain <ramadhani@tazkia.ac.id>
2019-10-16 15:28:57 +0700

    Update layout + SesiDto

---
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dto/SesiDto.java
@@ -0,0 +1,30 @@
+package id.ac.tazkia.akademik.aplikasiakademik.dto;
+
+import id.ac.tazkia.akademik.aplikasiakademik.entity.Ruangan;
+import lombok.Data;
+
+
+
+@Data
+public class SesiDto {
+
+   private String id;
+   private String ruangan1;
+   private String ruangan2;
+   private String ruangan3;
+   private String ruangan4;
+   private String hari;
+   private String dosen1;
+   private String dosen2;
+   private String dosen3;
+   private String dosen4;
+   private String kelas1;
+   private String kelas2;
+   private String kelas3;
+   private String kelas4;
+   private String sesi1;
+   private String sesi2;
+   private String sesi3;
+   private String sesi4;
+
+}
