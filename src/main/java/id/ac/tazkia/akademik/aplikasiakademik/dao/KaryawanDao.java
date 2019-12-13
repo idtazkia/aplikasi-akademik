d7176e2917c30a5d3c0fce7e78ebce403e147cec
gifar <gifar@tazkia.ac.id>
2019-12-13 08:53:03 +0700

    form tambah karyawan

--- src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KaryawanDao.java
+++ src/main/java/id/ac/tazkia/akademik/aplikasiakademik/dao/KaryawanDao.java
@@ -4,6 +4,8 @@ import id.ac.tazkia.akademik.aplikasiakademik.dto.ApiRfidDto;
 import id.ac.tazkia.akademik.aplikasiakademik.entity.Karyawan;
 import id.ac.tazkia.akademik.aplikasiakademik.entity.StatusRecord;
 import id.ac.tazkia.akademik.aplikasiakademik.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -17,5 +19,9 @@ public interface KaryawanDao extends PagingAndSortingRepository<Karyawan,String>
 
     @Query("select new id.ac.tazkia.akademik.aplikasiakademik.dto.ApiRfidDto(k.idAbsen,k.namaKaryawan,k.rfid,true ,'',0) from  Karyawan k where k.status = :status and k.rfid is not null")
     List<ApiRfidDto> rfidKaryawan(@Param("status")StatusRecord statusRecord);
+
+    Page<Karyawan> findByStatusOrderByNamaKaryawan(StatusRecord statusRecord, Pageable page);
+
+    Page<Karyawan> findByStatusAndNamaKaryawanContainingIgnoreCaseOrderByNamaKaryawan(StatusRecord statusRecord, String namaKaryawan, Pageable page);
 
 }
