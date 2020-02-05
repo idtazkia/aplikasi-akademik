5c89e87e4f010718b0c2355fecd728f34b504a30
gifar <gifar@tazkia.ac.id>
2020-02-05 16:43:31 +0700

    input dosen

--- src/main/java/id/ac/tazkia/smilemahasiswa/controller/HumanResourcesController.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/controller/HumanResourcesController.java
@@ -1,8 +1,11 @@
 package id.ac.tazkia.smilemahasiswa.controller;
 
-import id.ac.tazkia.smilemahasiswa.dao.KaryawanDao;
+import id.ac.tazkia.smilemahasiswa.dao.*;
+import id.ac.tazkia.smilemahasiswa.dto.human.DosenDto;
+import id.ac.tazkia.smilemahasiswa.entity.Dosen;
 import id.ac.tazkia.smilemahasiswa.entity.Karyawan;
 import id.ac.tazkia.smilemahasiswa.entity.StatusRecord;
+import id.ac.tazkia.smilemahasiswa.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -14,11 +17,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 
 @Controller
 public class HumanResourcesController {
     @Autowired
     private KaryawanDao karyawanDao;
+
+    @Autowired
+    private DosenDao dosenDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private ProdiDao prodiDao;
+
+    @Autowired
+    private RoleDao roleDao;
 
 //Employee
 
@@ -55,6 +71,126 @@ public class HumanResourcesController {
         karyawan.setStatus(StatusRecord.HAPUS);
         karyawan.setIdAbsen(karyawan.getIdAbsen());
         karyawanDao.save(karyawan);
+        return "redirect:list";
+    }
+
+//    Lecturer
+
+    @GetMapping("/humanResources/lecturer/list")
+    public void gedungList(Model model, Pageable page, String search) {
+
+        if (StringUtils.hasText(search)) {
+            model.addAttribute("search", search);
+            model.addAttribute("listDosen", dosenDao.findByStatusNotInAndKaryawanNamaKaryawanContainingIgnoreCaseOrKaryawanNikContainingIgnoreCase(Arrays.asList(StatusRecord.HAPUS), search,search,page));
+        } else {
+            model.addAttribute("listDosen", dosenDao.findByStatusNotIn(Arrays.asList(StatusRecord.HAPUS),page));
+        }
+    }
+
+    @GetMapping("/humanResources/lecturer/form")
+    public void   formDosen(Model model,@RequestParam(required = false) String id){
+        model.addAttribute("prodi", prodiDao.findByStatusNotIn(Arrays.asList(StatusRecord.HAPUS)));
+        model.addAttribute("karyawan", new DosenDto());
+
+        if (id != null && !id.isEmpty()) {
+            Karyawan karyawan = karyawanDao.findById(id).get();
+            if (karyawan != null) {
+                DosenDto dosenDto = new DosenDto();
+                dosenDto.setEmail(karyawan.getEmail());
+                dosenDto.setGelar(karyawan.getGelar());
+                dosenDto.setId(karyawan.getId());
+                dosenDto.setJenisKelamin(karyawan.getJenisKelamin());
+                dosenDto.setNama(karyawan.getNamaKaryawan());
+                dosenDto.setNidn(karyawan.getNidn());
+                dosenDto.setNik(karyawan.getNik());
+                dosenDto.setStatusDosen(dosenDao.findByKaryawan(karyawan).getStatusDosen());
+                dosenDto.setHonor(dosenDao.findByKaryawan(karyawan).getHonor());
+                dosenDto.setRfid(karyawan.getRfid());
+                dosenDto.setAbsen(karyawan.getIdAbsen());
+                if (karyawan.getIdUser() != null) {
+                    dosenDto.setIdUser(karyawan.getIdUser());
+                }
+                dosenDto.setProdi(dosenDao.findByKaryawan(karyawan).getProdi().getId());
+                dosenDto.setTanggalLahir(karyawan.getTanggalLahir());
+                model.addAttribute("karyawan", dosenDto);
+                model.addAttribute("dosen", dosenDao.findByKaryawan(karyawan));
+            }
+        }
+
+    }
+
+    @PostMapping("/humanResources/lecturer/form")
+    public String prosesForm(@Valid DosenDto dosenDto){
+        if (dosenDto.getId() == null || dosenDto.getId().isEmpty()) {
+            User user = new User();
+            user.setActive(Boolean.TRUE);
+            user.setRole(roleDao.findById("dosen").get());
+            user.setUsername(dosenDto.getEmail());
+            userDao.save(user);
+
+            Karyawan karyawan = new Karyawan();
+            karyawan.setEmail(dosenDto.getEmail());
+            karyawan.setGelar(dosenDto.getGelar());
+            karyawan.setJenisKelamin(dosenDto.getJenisKelamin());
+            karyawan.setNamaKaryawan(dosenDto.getNama());
+            karyawan.setNidn(dosenDto.getNidn());
+            karyawan.setIdUser(user);
+            karyawan.setNik(dosenDto.getNik());
+            karyawan.setRfid(dosenDto.getRfid());
+            karyawan.setTanggalLahir(dosenDto.getTanggalLahir());
+            karyawanDao.save(karyawan);
+
+            Dosen d = new Dosen();
+            d.setProdi(prodiDao.findById(dosenDto.getProdi()).get());
+            d.setKaryawan(karyawan);
+            d.setStatusDosen(dosenDto.getStatusDosen());
+            d.setHonor(dosenDto.getHonor());
+            d.setStatus(StatusRecord.AKTIF);
+            dosenDao.save(d);
+        }else {
+            Karyawan karyawan = karyawanDao.findById(dosenDto.getId()).get();
+            if (dosenDto.getIdUser() == null){
+                User user = new User();
+                user.setActive(Boolean.TRUE);
+                user.setRole(roleDao.findById("dosen").get());
+                user.setUsername(dosenDto.getEmail());
+                userDao.save(user);
+                karyawan.setIdUser(user);
+            }
+            if (dosenDto.getIdUser() != null){
+                karyawan.setIdUser(dosenDto.getIdUser());
+                User user = userDao.findById(dosenDto.getIdUser().getId()).get();
+                user.setUsername(dosenDto.getEmail());
+                userDao.save(user);
+            }
+            karyawan.setEmail(dosenDto.getEmail());
+            karyawan.setGelar(dosenDto.getGelar());
+            karyawan.setJenisKelamin(dosenDto.getJenisKelamin());
+            karyawan.setNamaKaryawan(dosenDto.getNama());
+            karyawan.setNidn(dosenDto.getNidn());
+            karyawan.setTanggalLahir(dosenDto.getTanggalLahir());
+            karyawan.setRfid(dosenDto.getRfid());
+            karyawan.setNik(dosenDto.getNik());
+            karyawan.setIdAbsen(dosenDto.getAbsen());
+
+            karyawanDao.save(karyawan);
+
+            Dosen dosen = dosenDao.findByKaryawan(karyawan);
+            dosen.setStatusDosen(dosenDto.getStatusDosen());
+            dosen.setHonor(dosenDto.getHonor());
+            dosen.setProdi(prodiDao.findById(dosenDto.getProdi()).get());
+            dosenDao.save(dosen);
+        }
+
+
+        return "redirect:list";
+    }
+
+    @PostMapping("/humanResources/lecturer/delete")
+    public String delete(@RequestParam Dosen dosen){
+        dosen.setStatus(StatusRecord.HAPUS);
+        dosenDao.save(dosen);
+
         return "redirect:list";
     }
 }
--- src/main/java/id/ac/tazkia/smilemahasiswa/dao/DosenDao.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dao/DosenDao.java
@@ -1,9 +1,8 @@
 package id.ac.tazkia.smilemahasiswa.dao;
 
-import id.ac.tazkia.smilemahasiswa.entity.Dosen;
-import id.ac.tazkia.smilemahasiswa.entity.Karyawan;
-import id.ac.tazkia.smilemahasiswa.entity.StatusRecord;
-import id.ac.tazkia.smilemahasiswa.entity.TahunAkademik;
+import id.ac.tazkia.smilemahasiswa.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +14,7 @@ public interface DosenDao extends PagingAndSortingRepository<Dosen, String> {
     Iterable<Dosen> cariDosen(@Param("status")StatusRecord statusRecord);
 
     Iterable<Dosen> findByStatusNotIn(List<StatusRecord> hapus);
+    Page<Dosen> findByStatusNotIn(List<StatusRecord> hapus, Pageable pageable);
 
     @Query(value = "SELECT c.id AS id_dosen, d.nama_karyawan AS nama, SUM(jumlah_sks)AS sks FROM jadwal AS a INNER JOIN matakuliah_kurikulum AS b ON a.id_matakuliah_kurikulum = b.id INNER JOIN dosen AS c ON a.id_dosen_pengampu = c.id INNER JOIN karyawan AS d ON c.id_karyawan=d.id WHERE a.id_tahun_akademik =?1 AND a.status='AKTIF' AND b.status='AKTIF' GROUP BY a.id_dosen_pengampu", nativeQuery = true)
     List<Object[]> listDosen(TahunAkademik tahunAkademik);
@@ -22,6 +22,9 @@ public interface DosenDao extends PagingAndSortingRepository<Dosen, String> {
     Dosen findByKaryawanRfid(String rfid);
 
     Dosen findByKaryawan(Karyawan karyawan);
+    Dosen findByKaryawanIdUser(User user);
 
     Long countDosenByStatus(StatusRecord aktif);
+
+    Page<Dosen> findByStatusNotInAndKaryawanNamaKaryawanContainingIgnoreCaseOrKaryawanNikContainingIgnoreCase(List<StatusRecord> asList, String search, String search1, Pageable page);
 }
