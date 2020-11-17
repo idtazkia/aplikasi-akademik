87e576dd554c454425dcc9922adca231231e3962
Gifar Haidar <gifar@tazkia.ac.id>
2020-11-17 19:23:44 +0700

    get resource file

--- src/main/java/id/ac/tazkia/smilemahasiswa/controller/StudiesActivityController.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/controller/StudiesActivityController.java
@@ -27,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -1694,7 +1695,7 @@ public class StudiesActivityController {
         IpkDto ipk = krsDetailDao.ipkTahunAkademik(mahasiswa,tahunAkademik.getKodeTahunAkademik());
         IpkDto ip =  krsDetailDao.ip(mahasiswa,tahunAkademik);
 
-        FileInputStream file = new FileInputStream(contohExcelKhs.getFile());
+        FileInputStream file = new FileInputStream(new ClassPathResource("sample/sampleKhs.xlsx").getFile());
 
         XSSFWorkbook workbook = new XSSFWorkbook(file);
         XSSFSheet sheet = workbook.getSheetAt(0);
