29a5305661976824f261e8be3d4f8161346f37cb
Sawrain <ramadhani.drasaw@gmail.com>
2021-02-05 16:22:29 +0700

    DTO penilaian

---
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dto/elearning/MdlGradeGradesDto.java
@@ -0,0 +1,20 @@
+package id.ac.tazkia.smilemahasiswa.dto.elearning;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Data
+public class MdlGradeGradesDto {
+
+    private String id;
+    private String idJadwal;
+    private String mahasiswa;
+    private BigInteger idBobotTugas;
+    private BigDecimal finalGrade;
+    private String status;
+    private BigDecimal nilaiAkhir;
+    private BigDecimal bobot;
+
+}
