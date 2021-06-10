731342822d590ea7a324a28478573cfb3c551fcc
msopian <muhammadsopian001@gmail.com>
2021-06-10 14:04:40 +0700

    fixing praaktif dan jenis tagihan dao

--- src/main/java/id/ac/tazkia/smilemahasiswa/dao/JenisTagihanDao.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dao/JenisTagihanDao.java
@@ -14,11 +14,11 @@ import java.util.List;
 
 public interface JenisTagihanDao extends PagingAndSortingRepository<JenisTagihan, String> {
 
-    Page<JenisTagihan> findByStatusNotInAndNamaContainingIgnoreCaseOrderByNama(List<StatusRecord> asList, String search, Pageable page);
+    Page<JenisTagihan> findByStatusNotInAndNamaContainingIgnoreCaseOrderByKode(List<StatusRecord> asList, String search, Pageable page);
 
-    Page<JenisTagihan> findByStatusNotIn(List<StatusRecord> asList, Pageable page);
+    Page<JenisTagihan> findByStatusNotInOrderByKode(List<StatusRecord> asList, Pageable page);
 
     List<JenisTagihan> findByStatusOrderByNama(StatusRecord statusRecord);
 
-    JenisTagihan findByNamaAndStatus(JenisTagihan jenisTagihan, StatusRecord statusRecord);
+    JenisTagihan findByKodeAndStatus(String kode, StatusRecord statusRecord);
 }
