a0c1fe5315c71fd26488bc33db3fb53dd6ede3c4
Gifar Haidar <gifar@tazkia.ac.id>
2021-07-26 13:44:35 +0700

    report nilai

--- src/main/java/id/ac/tazkia/smilemahasiswa/config/KonfigurasiSecurity.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/config/KonfigurasiSecurity.java
@@ -90,7 +90,7 @@ public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers("/graduation/seminar/list").hasAnyAuthority("VIEW_SUPERUSER", "VIEW_KPS", "VIEW_AKADEMIK1")
                 .antMatchers("/graduation/lecture/list").hasAnyAuthority("VIEW_DOSEN", "VIEW_KPS", "VIEW_WAREK2","VIEW_DOSEN_AKADEMIK")
                 .antMatchers("/graduation/lecture/sempro").hasAnyAuthority("VIEW_DOSEN", "VIEW_KPS", "VIEW_WAREK2","VIEW_DOSEN_AKADEMIK")
-                .antMatchers("/report/recapitulation/lecturer").hasAnyAuthority("VIEW_SUPERUSER", "VIEW_AKADEMIK1", "VIEW_AKADEMIK2")
+                .antMatchers("/report/recapitulation/lecturer").hasAnyAuthority("VIEW_SUPERUSER", "VIEW_AKADEMIK1", "VIEW_AKADEMIK2","VIEW_DOSEN_AKADEMIK")
                 .antMatchers("/report/recapitulation/bkd").hasAnyAuthority("VIEW_DOSEN", "VIEW_WAREK2", "VIEW_KPS","VIEW_DOSEN_AKADEMIK")
                 .antMatchers("/report/recapitulation/edom").hasAnyAuthority("VIEW_SUPERUSER", "VIEW_AKADEMIK1","VIEW_AKADEMIK2", "VIEW_KPS")
                 .antMatchers("/report/recapitulation/ipk").hasAnyAuthority("VIEW_SUPERUSER", "VIEW_AKADEMIK1","VIEW_AKADEMIK2", "VIEW_STAFF")
