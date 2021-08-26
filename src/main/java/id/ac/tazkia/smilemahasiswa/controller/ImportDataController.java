7d004e1a87128092118270a569554a13488c46ac
Gifar Haidar <gifar@tazkia.ac.id>
2021-08-26 09:19:13 +0700

    import data mahasiswa

--- src/main/java/id/ac/tazkia/smilemahasiswa/controller/ImportDataController.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/controller/ImportDataController.java
@@ -1,18 +1,27 @@
 package id.ac.tazkia.smilemahasiswa.controller;
 
 import id.ac.tazkia.smilemahasiswa.dao.MahasiswaDao;
+import id.ac.tazkia.smilemahasiswa.dao.UserDao;
 import id.ac.tazkia.smilemahasiswa.dto.ImportMahasiswaDto;
+import id.ac.tazkia.smilemahasiswa.dto.elearning.MdlGradeItemsDto;
 import id.ac.tazkia.smilemahasiswa.entity.Mahasiswa;
+import id.ac.tazkia.smilemahasiswa.entity.User;
 import id.ac.tazkia.smilemahasiswa.service.MahasiswaService;
 import id.ac.tazkia.smilemahasiswa.utility.UrlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequestMapping(path = UrlUtil.Import.MAHASISWA)
 @RestController
@@ -26,33 +35,38 @@ public class ImportDataController  {
     @Autowired
     private MahasiswaDao mahasiswaDao;
 
+    @Autowired
+    private UserDao userDao;
 
-    @GetMapping(value = "/{nim}")
+
+    @PostMapping(value = "/proses")
     @ResponseBody
-    public ResponseEntity<Object> getMahasiswaBaru(@PathVariable String nim) {
+    public ResponseEntity<Object> getMahasiswaBaru(@RequestBody ImportMahasiswaDto request) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        ImportMahasiswaDto response = restTemplate
-                .getForObject(apiUrl+"?nim="+nim, ImportMahasiswaDto.class);
+        User user = userDao.findById(request.getId()).get();
 
-        if (response.getNim() == null){
-            return new ResponseEntity<>("Data Tidak ditemukan", HttpStatus.NOT_FOUND);
-        }else {
-            Mahasiswa mahasiswa = mahasiswaDao.findByNim(response.getNim());
-            if (mahasiswa != null) {
-                return new ResponseEntity<>("Data Sudah Dilaporkan", HttpStatus.ALREADY_REPORTED);
-            }else {
-                save(response);
-                return new ResponseEntity<>("Data Tersimpan", HttpStatus.CREATED);
+
+        if (user != null) {
+            if (request.getNim() == null || request.getNim().equals("null")) {
+                System.out.println("Request Null");
+
+                return new ResponseEntity<>("Data Tidak ditemukan", HttpStatus.NOT_FOUND);
+            } else {
+                Mahasiswa mahasiswa = mahasiswaDao.findByNim(request.getNim());
+                if (mahasiswa != null) {
+                    System.out.println("Data Sudah Dilaporkan");
+
+                    return new ResponseEntity<>("Data Sudah Dilaporkan", HttpStatus.ALREADY_REPORTED);
+                } else {
+
+                    Mahasiswa response = mahasiswaService.importMahasiswa(request);
+                    return new ResponseEntity<>(response, HttpStatus.CREATED);
+                }
             }
+        }else {
+            return new ResponseEntity<>("Access Denied", HttpStatus.FORBIDDEN);
+
         }
-
-    }
-
-    @PostMapping
-    public ResponseEntity<Object> save(@Valid @RequestBody ImportMahasiswaDto request){
-        Mahasiswa response = mahasiswaService.importMahasiswa(request);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 
--- src/main/java/id/ac/tazkia/smilemahasiswa/dto/ImportMahasiswaDto.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dto/ImportMahasiswaDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -16,30 +17,50 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ImportMahasiswaDto implements Serializable {
+    @NotNull
     private String nama;
+    @NotNull
     private String angkatan;
+    @NotNull
     private String prodi;
+    @NotNull
     private String nim;
+    @NotNull
     private String jenjang;
+    @NotNull
     private String program;
+    @NotNull
     private JenisKelamin jenisKelamin;
+    @NotNull
     private String idAgama;
+    @NotNull
     private String tempatLahir;
+    @NotNull
     @Column(columnDefinition = "DATE")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate tanggalLahir;
+    @NotNull
     private String kabupaten;
+    @NotNull
     private String provinsi;
+    @NotNull
     private String negara;
+    @NotNull
     private String kewarganegaraan;
     private String nik;
     private String nisn;
+    @NotNull
     private String alamat;
+    @NotNull
     private String telepon;
+    @NotNull
     private String email;
     private String statusAktif;
     private String user;
+    @NotNull
     private String ayah;
+    @NotNull
     private String ibu;
+    private String id;
 
 }
