829ad346148b5b0427d489372f50d74e181699af
gfrhaidar <gifar@tazkia.ac.id>
2021-10-26 09:52:33 +0700

    update nilai oleh prodi

--- src/main/java/id/ac/tazkia/smilemahasiswa/dao/SeminarDao.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dao/SeminarDao.java
@@ -16,7 +16,7 @@ import java.util.List;
 public interface SeminarDao extends PagingAndSortingRepository<Seminar, String> {
     Seminar findByNoteAndStatusNotIn(Note note, List<StatusApprove> statusRecords);
     Seminar findByNoteAndStatus(Note note, StatusApprove statusRecords);
-    Page<Seminar> findByTahunAkademikAndNoteMahasiswaIdProdiAndStatusNotInOrderByStatusDescTanggalInputAsc(TahunAkademik tahunAkademik, Prodi prodi, List<StatusApprove> status, Pageable pageable);
+    Page<Seminar> findByTahunAkademikAndNoteMahasiswaIdProdiAndStatusNotInOrderByStatusSemproDescPublishDesc(TahunAkademik tahunAkademik, Prodi prodi, List<StatusApprove> status, Pageable pageable);
     List<Seminar> findByTahunAkademikAndNoteMahasiswaIdProdiAndStatusNotInOrderByStatusDescTanggalInputDesc(TahunAkademik tahunAkademik, Prodi prodi, List<StatusApprove> status);
     @Query(value = "select 'Jadwal_kuliah' as jenis, c.nama_matakuliah as keterangan, e.nama_karyawan as milik  from jadwal as a inner join matakuliah_kurikulum as b on a.id_matakuliah_kurikulum=b.id inner join matakuliah as c on b.id_matakuliah=c.id inner join dosen as d on a.id_dosen_pengampu=d.id inner join karyawan as e on d.id_karyawan=e.id where id_tahun_akademik=?1 and id_hari = ?2 and id_ruangan=?3 and a.status='AKTIF' and jam_mulai > ?4 and jam_mulai < ?5  or id_tahun_akademik=?1 and id_hari = ?2 and id_ruangan=?3 and a.status='AKTIF'  and jam_selesai > ?4 and jam_selesai < ?5 union select 'Seminar' as jenis, b.judul as keterangan, c.nama as milik from seminar as a inner join note as b on a.id_note=b.id inner join mahasiswa as c on b.id_mahasiswa=c.id where a.status='APPROVED' and tanggal_ujian=?6 and jam_mulai > ?4 and jam_mulai < ?5 and ruangan = ?3 or a.status='APPROVED' and tanggal_ujian=?6 and jam_selesai > ?4 and jam_selesai < ?5 and ruangan = ?3 limit ?7", nativeQuery = true)
     Object[] validasiJadwalSeminar(TahunAkademik tahunAkademik, Hari hari, Ruangan ruangan, LocalTime jamMulai, LocalTime jamSelesai, LocalDate tanggalUjian, Integer limit);
@@ -25,7 +25,7 @@ public interface SeminarDao extends PagingAndSortingRepository<Seminar, String> 
     List<Seminar> cariSeminar(@Param("ketua") Dosen ketua,@Param("dosen")Dosen dosen, @Param("dosen1") Dosen dosenNote,@Param("tahun") TahunAkademik tahunAkademik);
 
     List<Seminar> findByStatusAndKetuaPengujiAndTahunAkademikOrStatusAndDosenPengujiAndTahunAkademikOrStatusAndNoteDosenAndTahunAkademikOrderByStatusSemproDesc(StatusApprove statusApprove1,Dosen dosen,TahunAkademik tahunAkademik,StatusApprove statusApprove2,Dosen dosen2, TahunAkademik tahunAkademik2, StatusApprove statusApprove3,Dosen dosen3,TahunAkademik tahunAkademik3);
-    
+
     List<Seminar> findByStatusAndNoteDosenAndTahunAkademikOrderByStatusSemproDesc(StatusApprove statusApprove,Dosen dosen,TahunAkademik tahunAkademik);
 
     @Query(value = "select round(sum(ka+ua+pa),2) from seminar where id = ?1", nativeQuery = true)
---
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dto/graduation/SeminarDto.java
@@ -0,0 +1,29 @@
+package id.ac.tazkia.smilemahasiswa.dto.graduation;
+
+import id.ac.tazkia.smilemahasiswa.entity.StatusRecord;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class SeminarDto {
+    private StatusRecord jenis;
+    private BigDecimal ka;
+    private BigDecimal kb;
+    private BigDecimal kc;
+    private BigDecimal kd;
+    private BigDecimal ke;
+    private BigDecimal kf;
+    private BigDecimal ua;
+    private BigDecimal ub;
+    private BigDecimal uc;
+    private BigDecimal ud;
+    private BigDecimal ue;
+    private BigDecimal uf;
+    private BigDecimal pa;
+    private BigDecimal pb;
+    private BigDecimal pc;
+    private BigDecimal pd;
+    private BigDecimal pe;
+    private BigDecimal pf;
+}
