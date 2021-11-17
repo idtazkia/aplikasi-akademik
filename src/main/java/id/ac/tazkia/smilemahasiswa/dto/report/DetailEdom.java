674ec6707d9aa50f5d1e88e26c88bf3cb81fda80
gfrhaidar <gifar@tazkia.ac.id>
2021-11-17 14:40:56 +0700

    detail edom

---
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dto/report/DetailEdom.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.smilemahasiswa.dto.report;
+
+public interface DetailEdom {
+    String getKrs();
+    String getJadwal();
+    String getMatkul();
+    String getCourse();
+    String getJadwalDosen();
+    String getDosen();
+    String getKaryawan();
+    Integer getTotal();
+}
