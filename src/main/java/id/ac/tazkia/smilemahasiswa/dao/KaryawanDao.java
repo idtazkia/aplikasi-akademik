e5fa24b0854e9891a5424fb40c9c0243024e61c7
zenbook <suprayogi@tazkia.ac.id>
2022-03-02 13:53:15 +0700

    dto

--- src/main/java/id/ac/tazkia/smilemahasiswa/dao/KaryawanDao.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dao/KaryawanDao.java
@@ -1,5 +1,6 @@
 package id.ac.tazkia.smilemahasiswa.dao;
 
+import id.ac.tazkia.smilemahasiswa.dto.human.KaryawanIntDto;
 import id.ac.tazkia.smilemahasiswa.dto.machine.ApiRfidDto;
 import id.ac.tazkia.smilemahasiswa.entity.Karyawan;
 import id.ac.tazkia.smilemahasiswa.entity.StatusRecord;
@@ -30,5 +31,12 @@ public interface KaryawanDao extends PagingAndSortingRepository<Karyawan, String
 
     @Query(value = "UPDATE karyawan SET id_absen= FLOOR(RAND() * (9999 - 7777) + 999)", nativeQuery = true)
     Update updateIdAbsenKaryawan();
+
+    @Query(value = "select id as id, nik as nik, nama_karyawan as namaKaryawan, gelar as gelar, jenis_kelamin as jenisKelamin, status as status, id_user as idUser, nidn as nidn,email as email,\n" +
+            "tanggal_lahir as tanggalLahir, rfid as rfid, id_absen as idAbsen, foto as foto\n" +
+            "from karyawan where status = 'AKTIF'\n" +
+            "order by nama_karyawan asc", nativeQuery = true)
+    List<KaryawanIntDto> apiGetKaryawan();
+
 
 }
