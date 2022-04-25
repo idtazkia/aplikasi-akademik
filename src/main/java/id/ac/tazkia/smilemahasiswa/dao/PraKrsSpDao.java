06a77c8084848544e62b1767e6a387b30bd6fbe1
gfrhaidar <gifar@tazkia.ac.id>
2022-04-25 11:32:32 +0700

    fix ipk khs mahasiswa

--- src/main/java/id/ac/tazkia/smilemahasiswa/dao/PraKrsSpDao.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dao/PraKrsSpDao.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import javax.xml.ws.WebEndpoint;
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface PraKrsSpDao extends PagingAndSortingRepository<PraKrsSp, String> {
