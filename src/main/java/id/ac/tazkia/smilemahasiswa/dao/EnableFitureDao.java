ebe8953cbe4400d1842d9fb25082bb20388ef2c3
msopian <muhammadsopian001@gmail.com>
2022-06-07 15:05:43 +0700

    update enable fitur tugas akhir

--- src/main/java/id/ac/tazkia/smilemahasiswa/dao/EnableFitureDao.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/dao/EnableFitureDao.java
@@ -9,4 +9,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface EnableFitureDao extends PagingAndSortingRepository<EnableFiture,String> {
     EnableFiture findByMahasiswaAndFiturAndEnableAndTahunAkademik(Mahasiswa mhsw, StatusRecord uts, Boolean enabled, TahunAkademik byStatus);
     EnableFiture findByMahasiswaAndFiturAndEnable(Mahasiswa mhsw, StatusRecord uts, Boolean enabled);
+    EnableFiture findByMahasiswaAndFiturAndTahunAkademik(Mahasiswa mahasiswa, StatusRecord uts, TahunAkademik tahunAkademik);
+
 }
