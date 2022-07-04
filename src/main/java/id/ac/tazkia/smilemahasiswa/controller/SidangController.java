f6e1dae095cb96995c3ca0cb1d5a901139db5f5e
gfrhaidar <gifar@tazkia.ac.id>
2022-07-04 08:26:26 +0700

    validasi sidang s2

--- src/main/java/id/ac/tazkia/smilemahasiswa/controller/SidangController.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/controller/SidangController.java
@@ -253,10 +253,14 @@ public class SidangController {
             model.addAttribute("seminar",seminar);
             PeriodeWisuda periodeWisuda = periodeWisudaDao.findByStatus(StatusRecord.AKTIF);
 //            if (LocalDate.now().compareTo(periodeWisuda.getTutupSidang()) >= 0 && LocalDate.now().compareTo(periodeWisuda.getBukaSidang()) <= 0 ){
-            if (LocalDate.now().compareTo(LocalDate.parse("2022-07-05")) <= 0 ){
-                return "redirect:info";
-            }else {
+            if (jenjang.equals("02")) {
                 return "graduation/sidang/mahasiswa/pendaftaran";
+            }else {
+                if (LocalDate.now().compareTo(LocalDate.parse("2022-07-05")) <= 0) {
+                    return "redirect:info";
+                } else {
+                    return "graduation/sidang/mahasiswa/pendaftaran";
+                }
             }
         }
     }
