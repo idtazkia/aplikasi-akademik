ced0d7ed1489b9d5b6478c0d87266d67be0baace
gfrhaidar <gifar@tazkia.ac.id>
2022-08-31 13:31:40 +0700

    report wisuda  excel

--- src/main/java/id/ac/tazkia/smilemahasiswa/controller/TugasAkhirController.java
+++ src/main/java/id/ac/tazkia/smilemahasiswa/controller/TugasAkhirController.java
@@ -2,12 +2,19 @@ package id.ac.tazkia.smilemahasiswa.controller;
 
 import id.ac.tazkia.smilemahasiswa.dao.*;
 import id.ac.tazkia.smilemahasiswa.dto.graduation.WisudaDto;
+import id.ac.tazkia.smilemahasiswa.dto.transkript.DataTranskript;
 import id.ac.tazkia.smilemahasiswa.entity.*;
 import id.ac.tazkia.smilemahasiswa.service.CurrentUserService;
 import id.ac.tazkia.smilemahasiswa.service.MailService;
 import org.apache.commons.lang3.text.WordUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
@@ -27,10 +34,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.chrono.HijrahDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class TugasAkhirController {
@@ -70,6 +85,9 @@ public class TugasAkhirController {
 
     @Value("${upload.wisuda}")
     private String wisudaFolder;
+
+    @Value("classpath:/sample/wisuda.xlsx")
+    private Resource getContohExcelWisuda;
 
     @ModelAttribute("prodi")
     public Iterable<Prodi> prodi() {
@@ -185,7 +203,7 @@ public class TugasAkhirController {
 
     }
 
-    @GetMapping("/graduation/wisuda/form")
+        @GetMapping("/graduation/wisuda/form")
     public void daftarWisudaPasca(Model model, Authentication authentication){
         User user = currentUserService.currentUser(authentication);
         Mahasiswa mahasiswa = mahasiswaDao.findByUser(user);
@@ -556,16 +574,23 @@ public class TugasAkhirController {
 
         Wisuda waiting = wisudaDao.findByMahasiswaAndStatus(mahasiswa,StatusApprove.WAITING);
         if (waiting != null){
-            if (!pasca.isEmpty()){
+            if (pasca != null){
                 model.addAttribute("pasca", pasca);
+                model.addAttribute("waiting", waiting);
+            }else {
+                model.addAttribute("waiting", waiting);
             }
-            model.addAttribute("waiting", waiting);
         }else {
-            if (!pasca.isEmpty()){
+            if (pasca != null){
                 model.addAttribute("pasca", pasca);
+                model.addAttribute("data", wisudaDao.findFirstByMahasiswaAndStatus(mahasiswa, StatusApprove.REJECTED));
+                System.out.println(wisudaDao.findFirstByMahasiswaAndStatus(mahasiswa, StatusApprove.REJECTED));
+                model.addAttribute("reject", "reject");
+            }else {
+                model.addAttribute("data", wisudaDao.findFirstByMahasiswaAndStatus(mahasiswa, StatusApprove.REJECTED));
+                model.addAttribute("reject", "reject");
             }
-            model.addAttribute("data", wisudaDao.findFirstByMahasiswaAndStatus(mahasiswa, StatusApprove.REJECTED));
-            model.addAttribute("reject", "reject");
+
         }
 
     }
@@ -577,7 +602,11 @@ public class TugasAkhirController {
         if (wisuda != null){
             return "redirect:result?wisuda=" + wisuda.getId();
         }else {
-            return "redirect:waiting";
+            if (mahasiswa.getIdProdi().getIdJenjang().getId().equals("02")) {
+                return "redirect:waiting?pasca=aktif";
+            }else {
+                return "redirect:waiting";
+            }
         }
 
     }
@@ -682,5 +711,316 @@ public class TugasAkhirController {
         }
     }
 
+    @GetMapping("/report/wisuda")
+    public void wisudaExcel (@RequestParam(required = false) String nim, HttpServletResponse response) throws IOException {
+
+        InputStream file = getContohExcelWisuda.getInputStream();
+
+        List<Wisuda> wisuda = wisudaDao.findByStatusAndPeriodeWisuda(StatusApprove.APPROVED, periodeWisudaDao.findByStatus(StatusRecord.AKTIF));
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        workbook.setSheetName(workbook.getSheetIndex(sheet), "Data Wisuda");
+
+        Font manajemenFont = workbook.createFont();
+        manajemenFont.setItalic(true);
+        manajemenFont.setFontHeightInPoints((short) 10);
+        manajemenFont.setFontName("Cambria");
+
+        Font dataManajemenFont = workbook.createFont();
+        dataManajemenFont.setFontHeightInPoints((short) 10);
+        dataManajemenFont.setFontName("Cambria");
+
+        Font subHeaderFont = workbook.createFont();
+        subHeaderFont.setFontHeightInPoints((short) 10);
+        subHeaderFont.setFontName("Cambria");
+        subHeaderFont.setBold(true);
+
+        Font judulFont = workbook.createFont();
+        judulFont.setFontHeightInPoints((short) 10);
+        judulFont.setFontName("Cambria");
+        judulFont.setBold(true);
+
+        Font thesisFont = workbook.createFont();
+        thesisFont.setFontHeightInPoints((short) 10);
+        thesisFont.setFontName("Cambria");
+
+        Font symbolFont = workbook.createFont();
+        symbolFont.setFontHeightInPoints((short) 10);
+        symbolFont.setFontName("Cambria");
+
+        Font dataFont = workbook.createFont();
+        dataFont.setFontHeightInPoints((short) 10);
+        dataFont.setFontName("Cambria");
+
+        Font dataTableFont = workbook.createFont();
+        dataTableFont.setFontHeightInPoints((short) 10);
+        dataTableFont.setFontName("Cambria");
+
+        Font prestasiFont = workbook.createFont();
+        prestasiFont.setFontHeightInPoints((short) 10);
+        prestasiFont.setFontName("Cambria");
+
+        Font judulSkripsiFont = workbook.createFont();
+        judulSkripsiFont.setFontHeightInPoints((short) 10);
+        judulSkripsiFont.setFontName("Cambria");
+
+        Font dataKhsFont = workbook.createFont();
+        dataKhsFont.setFontHeightInPoints((short) 10);
+        dataKhsFont.setFontName("Cambria");
+
+        Font dataIpkFont = workbook.createFont();
+        dataIpkFont.setFontHeightInPoints((short) 10);
+        dataIpkFont.setFontName("Cambria");
+
+        Font penilaianFont = workbook.createFont();
+        penilaianFont.setFontHeightInPoints((short) 10);
+        penilaianFont.setFontName("Cambria");
+
+        Font prestasiFont2 = workbook.createFont();
+        prestasiFont2.setFontHeightInPoints((short) 10);
+        prestasiFont2.setFontName("Cambria");
+
+        Font dataPrestasiFont = workbook.createFont();
+        dataPrestasiFont.setFontHeightInPoints((short) 10);
+        dataPrestasiFont.setFontName("Cambria");
+
+        Font matkulFont = workbook.createFont();
+        matkulFont.setFontHeightInPoints((short) 10);
+        matkulFont.setFontName("Cambria");
+
+        Font dataFontNew = workbook.createFont();
+        dataFontNew.setFontHeightInPoints((short) 10);
+        dataFontNew.setFontName("Times New Roman");
+
+        Font prodiFont = workbook.createFont();
+        prodiFont.setUnderline(XSSFFont.U_DOUBLE);
+        prodiFont.setFontHeightInPoints((short) 10);
+        prodiFont.setFontName("Cambria");
+
+        Font  presFont = workbook.createFont();
+        presFont.setFontHeightInPoints((short) 10);
+        presFont.setFontName("Cambria");
+
+        Font ipFont = workbook.createFont();
+        ipFont.setBold(true);
+        ipFont.setItalic(true);
+        ipFont.setFontHeightInPoints((short) 10);
+        ipFont.setFontName("Cambria");
+
+        Font ipFontBorder = workbook.createFont();
+        ipFontBorder.setBold(true);
+        ipFontBorder.setItalic(true);
+        ipFontBorder.setFontHeightInPoints((short) 10);
+        ipFontBorder.setFontName("Cambria");
+
+        Font lectureFont = workbook.createFont();
+        lectureFont.setBold(true);
+        lectureFont.setFontName("Cambria");
+        lectureFont.setUnderline(XSSFFont.U_DOUBLE);
+        lectureFont.setFontHeightInPoints((short) 10);
+
+        Font nikFont = workbook.createFont();
+        nikFont.setBold(true);
+        nikFont.setFontName("Cambria");
+        nikFont.setFontHeightInPoints((short) 10);
+
+        CellStyle styleNik = workbook.createCellStyle();
+        styleNik.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleNik.setFont(nikFont);
+
+
+        CellStyle styleManajemen = workbook.createCellStyle();
+        styleManajemen.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleManajemen.setAlignment(HorizontalAlignment.CENTER);
+        styleManajemen.setFont(manajemenFont);
+
+        CellStyle styleDosen = workbook.createCellStyle();
+        styleDosen.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDosen.setFont(lectureFont);
+
+        CellStyle styleProdi = workbook.createCellStyle();
+        styleProdi.setBorderTop(BorderStyle.MEDIUM);
+        styleProdi.setBorderBottom(BorderStyle.MEDIUM);
+        styleProdi.setBorderLeft(BorderStyle.MEDIUM);
+        styleProdi.setBorderRight(BorderStyle.MEDIUM);
+        styleProdi.setFont(dataManajemenFont);
+
+        CellStyle styleSubHeader = workbook.createCellStyle();
+        styleSubHeader.setFont(subHeaderFont);
+        styleSubHeader.setAlignment(HorizontalAlignment.LEFT);
+        styleSubHeader.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        CellStyle styleJudul = workbook.createCellStyle();
+        styleJudul.setFont(judulFont);
+        styleJudul.setAlignment(HorizontalAlignment.LEFT);
+        styleJudul.setVerticalAlignment(VerticalAlignment.TOP);
+
+        CellStyle styleThesis = workbook.createCellStyle();
+        styleThesis.setFont(thesisFont);
+        styleThesis.setAlignment(HorizontalAlignment.LEFT);
+        styleThesis.setVerticalAlignment(VerticalAlignment.TOP);
+        styleThesis.setWrapText(true);
+
+
+        CellStyle styleData = workbook.createCellStyle();
+        styleData.setFont(dataFont);
+
+        CellStyle styleDataNew = workbook.createCellStyle();
+        styleDataNew.setFont(dataFontNew);
+        styleDataNew.setAlignment(HorizontalAlignment.CENTER);
+
+        CellStyle styleDataKhs = workbook.createCellStyle();
+        styleDataKhs.setAlignment(HorizontalAlignment.CENTER);
+        styleDataKhs.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDataKhs.setFont(dataKhsFont);
+
+        CellStyle styleIsiIpk = workbook.createCellStyle();
+        styleIsiIpk.setAlignment(HorizontalAlignment.CENTER);
+        styleIsiIpk.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleIsiIpk.setFont(dataIpkFont);
+
+        CellStyle stylePenilaian = workbook.createCellStyle();
+        stylePenilaian.setAlignment(HorizontalAlignment.CENTER);
+        stylePenilaian.setVerticalAlignment(VerticalAlignment.CENTER);
+        stylePenilaian.setFont(penilaianFont);
+
+        CellStyle styleDataTale = workbook.createCellStyle();
+        styleDataTale.setFont(dataTableFont);
+        styleDataTale.setAlignment(HorizontalAlignment.CENTER);
+        styleDataTale.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDataTale.setBorderRight(BorderStyle.THIN);
+        styleDataTale.setBorderLeft(BorderStyle.THIN);
+
+        CellStyle styleDataTale2 = workbook.createCellStyle();
+        styleDataTale2.setFont(dataTableFont);
+        styleDataTale2.setAlignment(HorizontalAlignment.CENTER);
+        styleDataTale2.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDataTale2.setBorderRight(BorderStyle.THIN);
+        styleDataTale2.setBorderBottom(BorderStyle.THIN);
+        styleDataTale2.setBorderLeft(BorderStyle.THIN);
+
+        CellStyle styleDataPrestasiAkademik = workbook.createCellStyle();
+        styleDataPrestasiAkademik.setFont(prestasiFont);
+        styleDataPrestasiAkademik.setAlignment(HorizontalAlignment.LEFT);
+        styleDataPrestasiAkademik.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDataPrestasiAkademik.setBorderBottom(BorderStyle.MEDIUM);
+        styleDataPrestasiAkademik.setBorderTop(BorderStyle.MEDIUM);
+        styleDataPrestasiAkademik.setBorderRight(BorderStyle.MEDIUM);
+        styleDataPrestasiAkademik.setBorderLeft(BorderStyle.MEDIUM);
+
+        CellStyle styleDataKhsTable = workbook.createCellStyle();
+        styleDataKhsTable.setAlignment(HorizontalAlignment.LEFT);
+        styleDataKhsTable.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDataKhsTable.setFont(matkulFont);
+        styleDataKhsTable.setBorderRight(BorderStyle.THIN);
+        styleDataKhsTable.setBorderLeft(BorderStyle.THIN);
+
+        CellStyle styleDataKhsTable2 = workbook.createCellStyle();
+        styleDataKhsTable2.setAlignment(HorizontalAlignment.LEFT);
+        styleDataKhsTable2.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleDataKhsTable2.setFont(matkulFont);
+        styleDataKhsTable2.setBorderBottom(BorderStyle.THIN);
+        styleDataKhsTable2.setBorderRight(BorderStyle.THIN);
+        styleDataKhsTable2.setBorderLeft(BorderStyle.THIN);
+
+
+        CellStyle stylePrestasiAkademik = workbook.createCellStyle();
+        stylePrestasiAkademik.setAlignment(HorizontalAlignment.LEFT);
+        stylePrestasiAkademik.setVerticalAlignment(VerticalAlignment.CENTER);
+        stylePrestasiAkademik.setFont(prestasiFont);
+
+        CellStyle styleSubHeader1 = workbook.createCellStyle();
+        styleSubHeader1.setAlignment(HorizontalAlignment.LEFT);
+        styleSubHeader1.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleSubHeader1.setFont(ipFont);
+
+        CellStyle styleJudulSkripsi = workbook.createCellStyle();
+        styleJudulSkripsi.setFont(judulSkripsiFont);
+        styleJudulSkripsi.setWrapText(true);
+
+        CellStyle styleSymbol = workbook.createCellStyle();
+        styleSymbol.setAlignment(HorizontalAlignment.RIGHT);
+        styleSymbol.setFont(symbolFont);
+
+        CellStyle styleTotal = workbook.createCellStyle();
+        styleTotal.setAlignment(HorizontalAlignment.LEFT);
+        styleTotal.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleTotal.setFont(ipFont);
+
+        CellStyle styleTotalBorder = workbook.createCellStyle();
+        styleTotalBorder.setAlignment(HorizontalAlignment.CENTER);
+        styleTotalBorder.setVerticalAlignment(VerticalAlignment.CENTER);
+        styleTotalBorder.setFont(ipFontBorder);
+        styleTotalBorder.setBorderBottom(BorderStyle.THIN);
+        styleTotalBorder.setBorderTop(BorderStyle.THIN);
+        styleTotalBorder.setBorderRight(BorderStyle.THIN);
+        styleTotalBorder.setBorderLeft(BorderStyle.THIN);
+
+        CellStyle styleIpk = workbook.createCellStyle();
+        styleIpk.setFont(prodiFont);
+        styleIpk.setAlignment(HorizontalAlignment.CENTER);
+        styleIpk.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        CellStyle stylePres = workbook.createCellStyle();
+        stylePres.setFont(presFont);
+        stylePres.setAlignment(HorizontalAlignment.CENTER);
+        stylePres.setVerticalAlignment(VerticalAlignment.CENTER);
+        stylePres.setBorderBottom(BorderStyle.MEDIUM);
+        stylePres.setBorderTop(BorderStyle.MEDIUM);
+        stylePres.setBorderRight(BorderStyle.MEDIUM);
+        stylePres.setBorderLeft(BorderStyle.MEDIUM);
+
+        CellStyle borderRight = workbook.createCellStyle();
+        borderRight.setBorderRight(BorderStyle.MEDIUM);
+
+        int dataWisuda = 5 ;
+        for (Wisuda data : wisuda) {
+            Row row = sheet.createRow(dataWisuda);
+//            sheet.addMergedRegion(new CellRangeAddress(rowNumSemester8,rowNumSemester8,2,5));
+//            sheet.addMergedRegion(new CellRangeAddress(rowNumSemester8,rowNumSemester8,8,9));
+
+            row.createCell(1).setCellValue(data.getMahasiswa().getNim());
+            row.createCell(2).setCellValue(data.getMahasiswa().getNama());
+            row.createCell(3).setCellValue(data.getMahasiswa().getTanggalLahir().toString());
+            row.createCell(4).setCellValue(data.getMahasiswa().getAyah().getNamaAyah());
+            if (data.getMahasiswa().getBeasiswa() != null) {
+                row.createCell(5).setCellValue(data.getMahasiswa().getBeasiswa().getNamaBeasiswa());
+                row.getCell(5).setCellStyle(styleDataKhsTable2);
+            }else {
+                row.createCell(5).setCellValue("-");
+                row.getCell(5).setCellStyle(styleDataKhsTable2);
+
+            }
+            row.createCell(6).setCellValue(data.getMahasiswa().getUkuranBaju());
+            row.createCell(7).setCellValue(data.getMahasiswa().getJudul());
+            row.createCell(8).setCellValue(data.getMahasiswa().getTitle());
+            row.createCell(9).setCellValue(data.getMahasiswa().getJenisKelamin().toString());
+            row.createCell(10).setCellValue(data.getMahasiswa().getTeleponSeluler());
+            row.createCell(11).setCellValue(data.getMahasiswa().getEmailPribadi());
+            row.createCell(12).setCellValue("3.22");
+            row.createCell(13).setCellValue("asdasdasdad");
+            row.getCell(1).setCellStyle(styleDataTale2);
+                row.getCell(2).setCellStyle(styleDataKhsTable2);
+                row.getCell(3).setCellStyle(styleDataKhsTable2);
+                row.getCell(4).setCellStyle(styleDataKhsTable2);
+                row.getCell(6).setCellStyle(styleDataTale2);
+                row.getCell(7).setCellStyle(styleDataTale2);
+                row.getCell(8).setCellStyle(styleDataTale2);
+                row.getCell(10).setCellStyle(styleDataTale2);
+                row.getCell(11).setCellStyle(styleDataTale2);
+                row.getCell(12).setCellStyle(styleDataTale2);
+                row.getCell(13).setCellStyle(styleDataTale2);
+            dataWisuda++;
+        }
+
+
+
+        String namaFile = "Data Wisuda";
+        String extentionX = ".xlsx";
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition","attachment; filename=\""+ namaFile  + extentionX +  "\"");
+        workbook.write(response.getOutputStream());
+        workbook.close();
+    }
 
 }
